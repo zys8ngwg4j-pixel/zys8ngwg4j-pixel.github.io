e5e0ff3bcddfd127effb5bfb0a06e56f76c0cd93
zys8ngwg4j-pixel <zys8ngwg4j@privaterelay.appleid.com>
2026-08-11 16:31:08 +0500

    Delete hit_the_roman directory

--- hit_the_roman/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(caches.open('hitroman-v1').then(function(cache) {
-        return cache.addAll(['.', 'index.html', 'manifest.json', 'bg_lab.png', 'buddy_head.png', 'buddy_head_hurt.png', 'coin_icon.png', 'weapon_bat.png', 'weapon_sword.png', 'weapon_bita.png', 'weapon_shocker.png', 'weapon_acid.png', 'weapon_shuriken.png', 'weapon_laser_sword.png']);
-    }));
-});
-self.addEventListener('fetch', function(e) {
-    e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
-});
